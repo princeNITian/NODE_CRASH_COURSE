8c26ffd91c57f2028c5712bc29d36f310f988ad2
Prince Raj <princeraj427@gmai.com>
2019-07-02 09:15:42 +0530

    public dir added

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-// const person = require("./person");
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-// console.log(person.name);
+const server = http.createServer((req,res) =>{
+if(req.url === '/'){
+    // res.writeHead(200,{'Content-Type':})
+    // res.end('<h1>Homepage</h>');
+    fs.readFile(path.join(__dirname,'public','index.html'), (err,content)=>{
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end(content);
+    })
 
-const Logger = require('./logger');
+}
 
-const logger = new Logger();
+    if (req.url === '/about') {
+        // res.writeHead(200,{'Content-Type':})
+        // res.end('<h1>Homepage</h>');
+        fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(content);
+        })
 
-logger.on('message', (data) => console.log('Called Listener ',data));
+    }
 
-logger.log('Hello World');
+    if (req.url === '/api/users') {
+        
+            const users = [
+                {name: 'Prince', age: 40},
+                {name: 'John', age:30}
+            ];
+
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(users));
+        
+
+    }
+
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server Running on ${PORT}`));
--- logger.js
+++ logger.js
@@ -11,4 +11,12 @@ class Logger extends EventEmitter {
 }
 
 module.exports = Logger;
-console.log(uuid.v4());
+//console.log(uuid.v4());
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener ', data));
+
+logger.log('Hello World');
